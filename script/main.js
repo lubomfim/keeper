9ac5ba38dee4e562d89b34fb2cf91d3870a7327e
Lucas Bomfim <lucasliko@hotmail.com>
2020-10-22 14:55:45 -0300

    Update css & JS

--- script/main.js
+++ script/main.js
@@ -23,16 +23,73 @@ $(function() {
   }, function() {
     $(detailsPop).fadeOut('slow')
   })
+
   // --- OPEN MODAL IN DETAILS BUTTON CLICK
   detailsButton.click(function() {
     modalDetails.show()
   })
 
+// SWIPER
 
-  
+  let mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  spaceBetween: 50,
+  slidesPerView: 5,
+  freeMode: true,  
+})
+
+// Adicionando tasks
+
+const mainKeepItBtn = $('.keep-it__btn')
+
+mainKeepItBtn.click(function() {
+  let entrada = $('.entrada__task').val()
+if (entrada.length === 0) {
+  alert('Insira algo')
+
+} else {
+  CriadorObjeto(entrada)
+}
+})
 
 
+// keep it modal
 
+$('.modal__details-btn').click(function() {
+  let task = $('#todo').val()
+  let time = $('#time').val()
+  let friend = $('#friend').val()
+
+  CriadorObjeto(task, time, friend)
+  $('.modal__details-container').hide()
+})
+
+
+function CriadorObjeto(task, data, friend) {
+  let infos = {
+    task: task,
+    data: data,
+    friend: friend,
+  }
+  localStorageF(infos)
+}
+
+let cofreTasks = new Array();
+
+function localStorageF(entrada) {
+  cofreTasks.push(entrada)
+
+  $(cofreTasks).each((index, elemento) =>{ 
+  localStorage.setItem(index, JSON.stringify(elemento))})
+ 
+  CriadorMiniBox(entrada)
+  }
+
+
+function CriadorMiniBox(entrada) {
+mySwiper.appendSlide($('<div class="swiper-slide">').append($('<div class="swiper__mini-box">').append(`<p class="mini-box__text">${entrada.task}</p>`)))
+
+}
 })
 
 
